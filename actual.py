c0cdeee9ce677178253bc2164daa78e8292246ac
phynesimataa-jpg <phynesimataa@gmail.com>
2026-05-03 13:16:07 +0200

    Update greeting message in home route

---
+++ actual.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Hello Simataa👋; how are you</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+ 
