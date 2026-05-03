9080474b0158ae6ddb809e4054233e8965065fa3
phynesimataa-jpg <phynesimataa@gmail.com>
2026-05-03 15:16:16 +0200

    Fix greeting message in home route

--- actual.py
+++ actual.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello Simataa👋; how are you</h1>"
+    return "<h1>Hello Simataa👋</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
